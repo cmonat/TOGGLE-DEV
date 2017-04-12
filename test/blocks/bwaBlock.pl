06cd312ddcc6ec18b51165e560e16e18625d0402
sravel <sebastien.ravel@cirad.fr>
2017-04-12 16:06:03 +0200

    add BWA-SW

--- test/blocks/bwaBlock.pl
+++ test/blocks/bwaBlock.pl
@@ -49,7 +49,7 @@ my $dataFastq="$toggle/data/testData/fastq/pairedTwoIndividusIrigin";
 
 
 print "\n\n#################################################\n";
-print "#### TEST gatk BaseRecalibrator\n";
+print "#### TEST BWWA bwaSw\n";
 print "#################################################\n";
 
 # Remove files and directory created by previous test
